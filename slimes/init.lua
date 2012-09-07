d918eba164add83b79ef4df4c6164d7a936b1ad1
Jeija <norrepli@gmail.com>
2012-09-07 13:37:54 +0200

    Behaviour finetuning

--- slimes/init.lua
+++ slimes/init.lua
@@ -1,5 +1,6 @@
-SLIME_SIZE = 0.5
+SLIME_SIZE = 0.4
 SLIME_BOX = math.sqrt(2*math.pow(SLIME_SIZE, 2))/2
+GRAVITY = 9.8
 
 minetest.register_entity("slimes:small",{
 	initial_properties = {
@@ -20,14 +21,15 @@ minetest.register_entity("slimes:small",{
 
 	timer = 6,
 	timer2 = 1,
-	timer3 = 0,
+	timer3 = 0, --regularly check if slime touches ground and possibly set x/z velocity/acceleration to 0
 	yaw = 0,
 	direction = {},
 	status = 2, --1 = jump, 2 = rotate
 	attracts_slime = true,
+	found_friend = false,
 
 	on_activate = function(self)
-		self.object:setacceleration({x = 0, y = -9.8, z = 0})
+		self.object:setacceleration({x = 0, y = -GRAVITY, z = 0})
 	end,
 
 	on_punch = function(self)
@@ -36,22 +38,23 @@ minetest.register_entity("slimes:small",{
 
 	on_step = function(self, dtime)
 		self.timer2 = self.timer2 + dtime
-		if self.status == 2 and self.timer2 > 0.5 then
+		if self.status == 2 and (self.timer2 >= 0.5) then
 			self.timer2 = 1.2
 			self.status = 1
 
 			local pos = self.object:getpos()
-			if slime_lonely(pos) and not minetest.env:find_node_near(pos, 32, "default:mese") then
+			if slime_lonely(pos) and not minetest.env:find_node_near(pos, 16, "default:mese") then
 				self.object:remove()
 			end
 			self.yaw = slime_getyaw(pos)
-			if self.yaw == nil then
-				local yaw = math.random() * math.pi
+			if self.yaw == false then
+				self.yaw = math.random() * 360
+				self.found_friend = false
 			else
-				self.timer = self.timer + 4
+				self.found_friend = true
 			end
 			self.object:setyaw(self.yaw)
-			--self.object:set_properties({automatic_rotate = 0})
+			self.object:set_properties({automatic_rotate = 0})
 			self.direction = {x = math.cos(self.yaw)*2, y = 6, z = math.sin(self.yaw)*2}
 		end
 
@@ -61,21 +64,25 @@ minetest.register_entity("slimes:small",{
 
 		if self.timer2 > 1.3 and self.object:getvelocity().y == 0 then
 			self.object:setvelocity(self.direction)
+			self.object:setacceleration({x = self.direction.x/5, y = -GRAVITY, z = self.direction.z/5})
 			self.timer2 = 0
 		end
 
-		if self.timer >= 6 and self.object:getvelocity().y == 0 then
+		if (self.timer >= 6 or (self.timer >= 1 and self.found_friend == true)) and self.object:getvelocity().y == 0 then
 			self.timer = 0
 			self.timer2 = 0
 
 			self.status = 2
-			--self.object:set_properties({automatic_rotate = math.pi * 8})
+			if not self.found_friend then
+				self.object:set_properties({automatic_rotate = math.pi * 8})
+			end
 		end
 
 		if self.timer3 > 0.07 then
 			vel = self.object:getvelocity()
 			if vel.y == 0 and (vel.x ~= 0 or vel.z ~= 0) then
 				self.object:setvelocity({x = 0, y = 0, z = 0})
+				self.object:setacceleration({x = 0, y = -GRAVITY, z = 0})
 			end
 			self.timer3 = 0
 		end
@@ -99,7 +106,7 @@ function approx(val1, val2, deviation)
 end
 
 function slime_getyaw (pos) -- get yaw, only if objects to follow nearby
-	objs = minetest.env:get_objects_inside_radius(pos, 5)
+	objs = minetest.env:get_objects_inside_radius(pos, 7)
 	for i, obj in ipairs(objs) do
 		if obj:is_player() or obj:get_luaentity().name == "slimes:small" then
 			local ppos = {x = obj:getpos().x - pos.x,
@@ -111,7 +118,7 @@ function slime_getyaw (pos) -- get yaw, only if objects to follow nearby
 			end
 		end
 	end
-	return nil
+	return false
 end
 
 minetest.register_abm({
